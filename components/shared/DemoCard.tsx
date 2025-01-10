7cfa48ce49226b4b141a64a18d3c4951d782b5de
Red-Rose515 <Albertchess990919@gmail.com>
2025-01-10 11:46:50 +0200

    fix back

--- components/shared/DemoCard.tsx
+++ components/shared/DemoCard.tsx
@@ -30,9 +30,7 @@ const DemoCard = ({
         style={{ minHeight: "70vh" }}
         className={cx(
           "relative flex w-full rounded-xl p-6 shadow",
-          // "bg-gradient-to-br from-pink-300 via-fuchsia-300 to-purple-400 dark:from-pink-800 dark:via-fuchsia-900 dark:to-purple-800"
-          // "bg-gradient-to-br from-pink-400 via-fuchsia-300 to-purple-400 dark:from-pink-800 dark:via-fuchsia-900 dark:to-purple-800",
-          "bg-gradient-to-br from-pink-300 via-fuchsia-200 to-purple-300 dark:from-pink-800 dark:via-fuchsia-900 dark:to-purple-800",
+          "bg-gradient-to-br from-black/30 via-fuchsia-100 to-black/30 dark:from-pink-800 dark:via-fuchsia-900 dark:to-purple-800",
           {
             [Variant.Default]: "items-center justify-center",
             [Variant.ItemsCenter]: "items-center",
